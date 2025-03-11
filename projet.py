e7f7ebf15286e54163680db34278dbe8facd87b1
Mohamed Taher Borgi <medtaherborgi@gmail.com>
2025-03-11 12:40:16 +0100

    Fusion de projets1 avec squash

--- projet.py
+++ projet.py
@@ -5,5 +5,14 @@
  return S
 =======
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('La somme est :', Som)
+=======
+if Data:
+	print('la somme est :', sum(Data))
+	print('le min est :', min(Data))
+	print('le max est :', max(Data))
+else:
+	print('dossier vide')
+>>>>>>> projets1
